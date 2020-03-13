14aae9d870e2b1c5676bc95e3ce64fe5dcbe08d8
Ivica janevski <janevski.ivica@outlook.com>
2020-03-13 16:37:30 +0100

    HomeWork2

---
+++ Ex.1-SumOfEven/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ex._1_SumOfEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] numbers = new double[6];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("Enter integer no.1");
+                numbers[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            double sum = 0;
+
+            foreach (double number in numbers)
+            {
+                //Console.WriteLine($"Array numbers include this elements:{number}");
+                sum += number;
+                //Console.WriteLine($"The sum of arr numbers is{sum}");
+            }
+
+            double average = sum / 3;
+            Console.WriteLine($"The average is : {Math.Round(average, 2) }");
+            Console.Read();
+
+
+
+        }
+    }
+}
---
+++ Ex.2 StudentGroup/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Ex._2_StudentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] studentsG1 = { "joe", "vanesa", "bob", "steven" };
+            string[] studentsG2 = { "mike", "joane", "luke", "david" };
+
+
+            Console.WriteLine("Enter student group: ( there are 1 and 2)");
+
+            string userImput = Convert.ToString(Console.ReadLine());
+
+            if (userImput == "1")
+            {
+                Console.WriteLine($"The Students in G1 are:");
+                foreach (string student1 in studentsG1)
+                {
+
+                    //Console.WriteLine($"{student1}");
+
+                    Console.WriteLine(char.ToUpper(student1[0]) + student1.Substring(1));
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine($"The Students in G2 are:");
+                foreach (string student2 in studentsG2)
+                {
+                    //Console.WriteLine($" {student2}");
+
+                    Console.WriteLine(char.ToUpper(student2[0]) + student2.Substring(1));
+
+
+
+                }
+
+            }
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
